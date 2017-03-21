291b7f6a2d964bfdb976e3521a328177f4c61283
Jason Sherman <jsnshrmn@users.noreply.github.com>
2017-03-21 15:55:35 -0500

    require apt-get update before installing packages.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,15 @@
 class twlight::install inherits twlight {
 
+  # execute 'apt-get update'
+  exec { 'apt-update':                    # exec resource named 'apt-update'
+    command => '/usr/bin/apt-get update'  # command this resource will run
+  }
+
   # Install our packages
   if $twlight::package_manage {
     package { $package_name:
-      ensure => $package_ensure,
+      ensure  => $package_ensure,
+      require => Exec['apt-update']
     }
   }
 
@@ -14,12 +20,13 @@ class twlight::install inherits twlight {
     service_name            => 'mysql',
     root_password           => $mysqlroot,
     remove_default_accounts => true,
+    require                 => Exec['apt-update']
   }
 
   # Install and config mariadb client using another module
   class {'::mysql::client':
     package_name   => 'mariadb-client',
     package_ensure => 'present',
+    require        => Exec['apt-update']
   }
 }
-
