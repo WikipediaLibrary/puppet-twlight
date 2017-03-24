62683284646959587b990d2b7d02e7dbb8a73d71
Jason Sherman <jsn.sherman@gmail.com>
2017-03-24 11:27:02 -0500

    Configure nginx site.

--- manifests/configapp.pp
+++ manifests/configapp.pp
@@ -1,5 +1,19 @@
 class twlight::configapp inherits twlight {
 
+  file { '/etc/nginx/sites-available/twlight':
+    ensure  => file,
+    content => template('twlight/nginx.conf.twlight.erb'),
+    owner   => '33',
+    group   => '33',
+    mode    => '0644',
+  }
+
+  file { '/etc/nginx/sites-enabled/twlight':
+    ensure  => 'link',
+    target  => '/etc/nginx/sites-available/twlight',
+    force   => true,
+  }
+
   file { '/var/www/html/TWLight/TWLight/settings/production_vars.py':
     ensure  => file,
     content => template('twlight/production_vars.py.erb'),
