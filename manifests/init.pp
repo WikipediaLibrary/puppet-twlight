29c6b5e62ed684b0ddb0e095696d05f7874a4427
Jason Sherman <jsn.sherman@gmail.com>
2018-06-08 15:38:40 -0500

    Modern puppet actually does class containment right, so we can stop using the anchor pattern in the wrapper.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,31 +1,14 @@
 # == Class: twlight
 #
-# Full description of class twlight here.
-#
 # === Parameters
 #
 # Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream twlight servers as an array."
 #
 # === Variables
 #
 # Here you should define a list of variables that this module would require.
 #
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
 # === Examples
-#
-#  class { 'twlight':
-#    servers => [ 'pool.twlight.org', 'twlight.local.company.com' ],
-#  }
 #
 # === Authors
 #
@@ -60,16 +43,17 @@ class twlight (
   Array     $package_name = $twlight::params::package_name,
 ) inherits twlight::params {
 
-  # Anchor this as per #8040 - this ensures that classes won't float off and
-  # mess everything up.  You can read about this at:
-  # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
-  anchor { 'twlight::begin': } ->
-  class { '::twlight::repo': } ->
-  class { '::twlight::install': } ->
-#  class { '::twlight::tidymariadb': } ->
-  class { '::twlight::configsys': } ->
-  class { '::twlight::fetch': } ->
-  class { '::twlight::configapp': } ->
-  class { '::twlight::handler': } ->
-  anchor { 'twlight::end': }
+  contain ::twlight::repo
+  contain ::twlight::install
+  contain ::twlight::configsys
+  contain ::twlight::fetch
+  contain ::twlight::configapp
+  contain ::twlight::handler
+
+  Class['::twlight::repo'] ->
+  Class['::twlight::install'] ->
+  Class['::twlight::configsys'] ->
+  Class['::twlight::fetch'] ->
+  Class['::twlight::configapp'] ->
+  Class['::twlight::handler']
 }
